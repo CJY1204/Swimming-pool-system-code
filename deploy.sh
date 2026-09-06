d701f9542b93cd46ba6f3a139006d9ea7fcc11f5
CJY1204 <junyuc1204@gmail.com>
2026-09-06 17:55:19 +0800

    Update deployment script for new repository

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo "Start Deploy..."
+echo "Start deploy..."
 
 cd /tmp
-curl -L https://github.com/CJY1204/Cloud-Computing-For-Business-Assignment/archive/refs/heads/main.zip -o code.zip
+curl -L https://github.com/CJY1204/Swimming-pool-system-code/archive/refs/heads/main.zip -o code.zip
 unzip -o code.zip
 
 sudo rm -rf /var/www/html/*
-sudo cp -r Cloud-Computing-For-Business-Assignment-main/pool-booking-php/* /var/www/html/
+sudo cp -r Cloud-Computing-For-Business-Assignment-main/* /var/www/html/
 sudo chown -R apache:apache /var/www/html/
 sudo chmod -R 755 /var/www/html/
 
 rm -rf code.zip Cloud-Computing-For-Business-Assignment-main
 
-echo "Deploy Done！"
+echo "Deploy complete！"
