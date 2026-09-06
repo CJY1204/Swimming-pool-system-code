08643e1e09f8a951ea7882de5dc0ac060eecd9cb
CJY1204 <junyuc1204@gmail.com>
2026-09-06 17:58:41 +0800

    Update deployment script for new project structure

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-echo "Start deploy..."
+echo "Start Deploy..."
 
 cd /tmp
 curl -L https://github.com/CJY1204/Swimming-pool-system-code/archive/refs/heads/main.zip -o code.zip
 unzip -o code.zip
 
 sudo rm -rf /var/www/html/*
-sudo cp -r Cloud-Computing-For-Business-Assignment-main/* /var/www/html/
+sudo cp -r Swimming-pool-system-code-main/* /var/www/html/
+sudo rm -f /var/www/html/deploy.sh
 sudo chown -R apache:apache /var/www/html/
 sudo chmod -R 755 /var/www/html/
 
-rm -rf code.zip Cloud-Computing-For-Business-Assignment-main
+rm -rf code.zip Swimming-pool-system-code-main
 
-echo "Deploy complete！"
+echo "Deploy Complete！"
